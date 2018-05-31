ff77fa33d1f2d9d4e7db1b57e1d0f9ca6bb7fce4
Adrian Tokarski <istquai102@gmail.com>
2018-05-31 13:08:42 +0200

    Update app.js

--- js/app.js
+++ js/app.js
@@ -1,66 +1,83 @@
-$(document).ready(function() {
+$(document).ready(function () {
+    var rootDiv = $("#root");
+    refreshBooks(rootDiv);
+    handleForm();
 
-	var rootDiv = $('#root');
-	refreshBook(rootDiv);
-	handleForm();
+    rootDiv.on('click', '.book', function () {
+        console.log($(this).data("id"));
+        var bookDiv = $(this);
+        var detailDiv = bookDiv.find('div');
+        var bookId = $(this).data("id");
 
-	rootDiv.on('click', '.book', function(){
-		console.log($(this).data("id"));
-		var bookDiv = $(this);
-		var detailDiv = bookDiv.find('div');
-		var bookId = $(this).data('id');
+        $.ajax({
+            url: "http://localhost:8282/books/" + bookId,
+            type: "GET"
+        }).done(function (bookDetails) {
+            detailDiv.toggle();
+            detailDiv.text("Autor: " + bookDetails.author + ", id " + bookDetails.id + ", isbn " + bookDetails.isbn + ", publisher "
+                + bookDetails.publisher + ", type " + bookDetails.type);
+        });
 
-		$.ajax({
-			url: "http://localhost:8282/books/" + bookId,
-			type: "GET"
-		}).done(function(bookDetails){
-			detailDiv.toggle();
-			detailDiv.text("Author: " + bookDetails.author + ", ID: " + bookDetails.id + ", ISBN: " + bookDetails.isbn + ", publisher: " + bookDetails.publisher + ", type: " + bookDetails.type);
+        rootDiv.on('click', '.delete-button', function(event) {
+            event.stopPropagation();
+            var bookId = $(this).parent().data("id");
+            $.ajax({
+                url: "http://localhost:8282/books/" + bookId,
+                type: "DELETE"
+            }).done(function () {
+                refreshBooks(rootDiv);
+            })
+        })
 
-		});
-	})
+    });
+    
+
+    function refreshBooks(rootElement) {
+        rootElement.html("");
+        $.ajax({
+            url: "http:/localhost:8282/books",
+            type: "GET"
+        }).done(function (data) {
+            for (var i = 0; i < data.length; i++) {
+                var bookElement =
+                    $("<div class='book' data-id='" + data[i].id + "'>" 
+                    + data[i].title 
+                    + "<button class='delete-button'> Usuń</button>"
+                    + "<div style='display: none; color: blue;'></div></div>");
+                rootElement.append(bookElement);
+            };
+        });
+    }
+
+    function handleForm() {
+        var form = $('.new_book');
+        var submitButton = form.find('#add-button');
+        submitButton.on('click', function(event) {
+            event.preventDefault();
+            
+            var newBook = {};
+
+            newBook.author = $('#author').val();
+            newBook.isbn = $('#isbn').val();
+            newBook.publisher = $('#wydawca').val();
+            newBook.title = $('#title').val();
+            newBook.type = $('#type').val();
+
+            $.ajax({
+                url: "http://localhost:8282/books",
+                type: "POST",
+                headers: {
+                    "Accept": "application/json",
+                    "Content-Type": "application/json"
+                },
+                data: JSON.stringify(newBook)
+            }).done(function(){
+                refreshBooks($('#root'));
+            })
+
+            //shift + option + arrow up sublime 
+            
+        })
+    }
 
 });
-
-function handleForm(){
-	var form = $('.new_book');
-	var submitButton = form.find('#add-button');
-	submitButton.on('click', function(event){
-		event.preventDefault();
-
-		var newBook= {};
-
-		newBook.author = $('#author').val();
-		newBook.isbn = $('#isbn').val();
-		newBook.publisher = $('#publisher').val();
-		newBook.title = $('#title').val();
-		newBook.type = $('#type').val();
-
-		$.ajax({
-			url: "http://localhost:8282/books",
-			type: "POST",
-			headers: {
-				'Accept': 'application/json',
-				"Content-Type": "application/json"
-			},
-			data: JSON.stringify(newBook)
-		}).done(function(){
-			refreshBook($('#root'))
-		})
-	});
-}
-
-function refreshBook(rootElement){
-	rootElement.html("");
-	$.ajax({
-		url: "http://localhost:8282/books"
-	}).done(function(data){
-
-	
-		for(var i=0; i < data.length; i++){
-			var bookElement = $("<div class='book' data-id='" + data[i].id + "'>" + data[i].title + "<div style='display: none; background-color: azure;'></div></div>");
-			rootElement.append(bookElement);
-		}
-
-	})
-}
